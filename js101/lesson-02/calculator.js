04ed4b319634862d69420d7ddfb7ebf78ecfd2bb
Florence Shelley <smc.florence@gmail.com>
2020-01-06 15:29:10 -0500

    Refactor to use a single 'get input' method

--- js101/lesson-02/calculator.js
+++ js101/lesson-02/calculator.js
@@ -1,6 +1,9 @@
 const {question} = require('readline-sync');
 const {localize} = require('./calculator-localization');
 
+const LANGUAGES = ['en', 'fr'],
+      OPERATORS = ['1', '2', '3', '4'],
+      DECISIONS = ['y', 'n'];
 let lang = 'en';
 
 // Get prompt messages based on language
@@ -12,69 +15,49 @@ const prompt = (key, extension = '', userLang = lang) => {
 // Check for valid number
 const isInvalidNum = num => !num || Number.isNaN(Number(num));
 
-// Ask the user to select a language
-const getLang = () => {
-  const LANGUAGES = ['en', 'fr'];
-
-  prompt('langSelect', '', 'en');
-  let userLang = question().toLowerCase();
-
-  while (!LANGUAGES.includes(userLang)) {
-    prompt('invalidLanguage');
-    userLang = question().toLowerCase();
+// Prompt for valid number
+const getValidNum = (input, error) => {
+  let newInput = input;
+  while (isInvalidNum(newInput)) {
+    prompt(error);
+    newInput = question();
   }
-
-  return userLang;
+  return newInput;
 };
 
-// Ask the user for a number
-const getNum = position => {
-  prompt(`${position}Number`);
-  let number = question();
-
-  while (isInvalidNum(number)) {
-    prompt('invalidNumber');
-    number = question();
+// Prompt for valid option
+const getValidOption = (options, input, error) => {
+  let newInput = input;
+  while (!options.includes(newInput)) {
+    prompt(error);
+    newInput = question().toLowerCase();
   }
-
-  return Number(number);
+  return newInput;
 };
 
-// Ask the user for an operation to perform
-const getOperator = () => {
-  const OPERATORS = ['1', '2', '3', '4'];
+// Ask for user input
+const getUserInput = (options = [], message = '', error = '') => {
+  prompt(message);
+  let input = question().toLowerCase();
 
-  prompt('operationType');
-  let operator = question();
-
-  while (!OPERATORS.includes(operator)) {
-    prompt('invalidOperator');
-    operator = question();
+  // If no array of options is passed in, check for valid number
+  if (!options) {
+    input = getValidNum(input, error);
+  } else {
+    input = getValidOption(options, input, error);
   }
 
-  return operator;
-};
-
-// Ask the user if they want to calculate something else
-const getUserDecision = () => {
-  prompt('performAnother');
-  let decision = question().toLowerCase();
-
-  while (!['y', 'n'].includes(decision)) {
-    prompt('invalidYesOrNo');
-    decision = question().toLowerCase();
-  }
-
-  return decision;
+  // If entered value is a string of integers, return as a number value
+  return !isInvalidNum(input) ? Number(input) : input;
 };
 
 // Perform the operation on the two numbers
 const calculate = (num1, num2, operator) => {
   switch (operator) {
-    case '1': return num1 + num2;
-    case '2': return num1 - num2;
-    case '3': return num1 * num2;
-    case '4': return num1 / num2;
+    case 1: return num1 + num2;
+    case 2: return num1 - num2;
+    case 3: return num1 * num2;
+    case 4: return num1 / num2;
     default: return localize('noResult', lang);
   }
 };
@@ -88,23 +71,23 @@ const displayResult = output => (
 
 // Main
 const calculator = () => {
-  let num1 = getNum('first');
-  let num2 = getNum('second');
-  let operator = getOperator();
+  let num1 = getUserInput(null, 'firstNumber', 'invalidNumber');
+  let num2 = getUserInput(null, 'secondNumber', 'invalidNumber');
+  let operator = getUserInput(OPERATORS, 'operationType', 'invalidOperator');
   let output = calculate(num1, num2, operator);
   displayResult(output);
 
-  let calculateAgain = getUserDecision();
-  if (calculateAgain === 'y') {
+  let runAgain = getUserInput(DECISIONS, 'performAnother', 'invalidDecision');
+  if (runAgain === 'y') {
     calculator();
-  } else if (calculateAgain === 'n') {
+  } else if (runAgain === 'n') {
     prompt('terminate');
   }
 };
 
 // Initialize
 (() => {
-  lang = getLang();
+  lang = getUserInput(LANGUAGES, 'langSelect', 'invalidLanguage');
   prompt('greeting');
   calculator();
 })();
