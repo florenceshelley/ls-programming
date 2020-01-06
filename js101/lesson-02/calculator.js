3e7059d6288c3acc8b00710a4b57b6f600cf0411
Florence Shelley <smc.florence@gmail.com>
2020-01-05 23:21:59 -0500

    Refactor calculator and add ability to ask for another calculation

--- js101/lesson-02/calculator.js
+++ js101/lesson-02/calculator.js
@@ -1,54 +1,84 @@
 const {question} = require('readline-sync');
-let num1, num2, operator, output;
 
 const prompt = message => console.log(`=> ${message}`);
 const invalidNum = num => !num || Number.isNaN(Number(num));
 
-prompt('Welcome to Calculator!');
+// Ask the user for a number
+const getNum = position => {
+  prompt(`What's the ${position} number?`);
+  let number = question();
 
-// Ask the user for the first number
-prompt('What\'s the first number?');
-num1 = question();
+  while (invalidNum(number)) {
+    prompt('Whoops! You\'ve entered an invalid number value');
+    number = question();
+  }
 
-while (invalidNum(num1)) {
-  prompt('Whoops! You\'ve entered an invalid number value');
-  num1 = question();
-}
-
-// Ask the user for the second number
-prompt('What\'s the second number?');
-num2 = question();
-
-while (invalidNum(num2)) {
-  prompt('Whoops! You\'ve entered an invalid number value');
-  num2 = question();
-}
+  return number;
+};
 
 // Ask the user for an operation to perform
-prompt('What operation would you like to perform?');
-prompt('1) Add 2) Subtract 3) Multiply 4) Divide');
-operator = question();
+const getOperator = () => {
+  const OPERATORS = ['1', '2', '3', '4'];
 
-while (!['1', '2', '3', '4'].includes(operator)) {
-  prompt('Whoops! You\'ve selected an invalid operation');
-  operator = question();
-}
+  prompt('What operation would you like to perform?');
+  prompt('1) Add 2) Subtract 3) Multiply 4) Divide');
+  let operator = question();
+
+  while (!OPERATORS.includes(operator)) {
+    prompt('Whoops! You\'ve selected an invalid operation');
+    operator = question();
+  }
+
+  return operator;
+};
 
 // Perform the operation on the two numbers
-switch (operator) {
-  case '1':
-    output = Number(num1) + Number(num2);
-    break;
-  case '2':
-    output = Number(num1) - Number(num2);
-    break;
-  case '3':
-    output = Number(num1) * Number(num2);
-    break;
-  case '4':
-    output = Number(num1) / Number(num2);
-    break;
-}
+const calculate = (num1, num2, operator) => {
+  let output;
 
-// Print the result to the terminal
-prompt(`The result is: ${output}`);
+  switch (operator) {
+    case '1':
+      output = Number(num1) + Number(num2);
+      break;
+    case '2':
+      output = Number(num1) - Number(num2);
+      break;
+    case '3':
+      output = Number(num1) * Number(num2);
+      break;
+    case '4':
+      output = Number(num1) / Number(num2);
+      break;
+  }
+
+  // Print the result to the terminal
+  prompt(`The result is: ${output}`);
+};
+
+// Main
+const calculator = () => {
+  let num1 = getNum('first');
+  let num2 = getNum('second');
+  let operator = getOperator();
+
+  calculate(num1, num2, operator);
+
+  // Ask the user if they want to calculate something else
+  prompt('Do you want to perform another calculation? (y/n)');
+  let calculateAgain = question().toLowerCase();
+
+  while (!['y', 'n'].includes(calculateAgain)) {
+    prompt ('Whoops! Please enter yes (y) or no (n)');
+    calculateAgain = question().toLowerCase();
+  }
+
+  if (calculateAgain === 'y') {
+    calculator();
+  } else if (calculateAgain === 'n') {
+    prompt('Goodbye!');
+  }
+};
+
+// Initialize
+prompt('Welcome to Calculator!');
+calculator();
