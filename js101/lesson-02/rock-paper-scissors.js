97832d73f2eb77985e2ebb340c897197facc1f20
Florence Shelley <smc.florence@gmail.com>
2020-02-18 23:34:44 -0500

    Initialize rps game

---
+++ js101/lesson-02/rock-paper-scissors.js
@@ -0,0 +1,23 @@
+/**
+ * Rock Paper Scissors
+ * @author: Florence Shelley
+ *
+ * Rock Paper Scissors is a simple game played between two opponents.
+ * Both the opponents choose an item from rock, paper, and scissors.
+ * The winner is decided according to the following rules:
+ *
+ * 1. If player A chooses rock and player B chooses scissors, player A wins.
+ * 2. If player A chooses paper and player B chooses rock, player A wins.
+ * 3. If player A chooses scissors and player B chooses paper, player A wins.
+ * 4. If both players choose the same item, neither players wins. It's a tie.
+ *
+ * This version of the game lets the user play against the computer.
+ * The game flow should go like this:
+ *
+ * 1. The user makes a choice.
+ * 2. The computer makes a choice.
+ * 3. The winner is displayed.
+ *
+ */
+
+
