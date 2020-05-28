b48260b3c3d143bba27b6d7b658a1f1508ad8b46
Florence Shelley <smc.florence@gmail.com>
2020-05-28 02:08:10 -0400

    Add TTT file and pseudocode

---
+++ js101/lesson-06/tic-tac-toe-decomposition.js
@@ -0,0 +1,24 @@
+/**
+ * Tic Tac Toe
+ * @author Florence Shelley
+ * @summary A Tic Tac Toe game where a user can play against the computer
+ *
+ * @description Tic Tac Toe is a 2-player game played on a 3x3 grid called the board.
+ * Each player takes a turn and marks a square on the board. The first player gets
+ * 3 squares in a row-horizontal, vertical, or diagonal-wins. If all 9 squares are
+ * filled and neither player has 3 in a row, the game is a tie.
+ */
+
+// TODO:
+// 1. Display the initial empty 3x3 board
+// 2. Ask the user to mark a square
+// 3. Computer marks a square
+// 4. Display the updated board state
+// 5. If it's a winning board, display the winner
+// 6. If the board is full, display tie
+// 7. If neither player won and the board is not full, go to #2
+// 8. Play again?
+// 9. If yes, go to #1
+// 10. Goodbye!
+
+ 
